e89e33349c89f77ff1bb3d22c184dd111bc7574f
richardxu1983 <2622619554@qq.com>
2016-09-21 21:10:15 +0800

    first update

---
+++ Assets/scripts/CameraMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float m_cameraMax = 8;
+    public float m_cameraMin = 3;
+    public float m_cameraMoveSpeed = 1.5f;
+    public float m_cameraScrollSpeed = 1.5f;
+
+    private float m_cameraScroll;
+    private Camera m_camera;
+    private float m_cameraCurSize;
+
+    private Vector3 m_lastMousePos;
+    private Vector3 m_curMousePos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CameraScroll();
+        CameraMove();
+    }
+
+    //
+    void FixedUpdate()
+    {
+
+    }
+
+    //
+    void CameraMove()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            //Debug.Log("Pressed middle click.");
+            m_lastMousePos.x = Input.mousePosition.x;
+            m_lastMousePos.z = Input.mousePosition.y;
+        }
+        else if(Input.GetMouseButton(2))
+        {
+            m_curMousePos.x = Input.mousePosition.x;
+            m_curMousePos.z = Input.mousePosition.y;
+            transform.position -= (m_curMousePos - m_lastMousePos) * Time.deltaTime * m_cameraMoveSpeed;
+            m_lastMousePos = m_curMousePos;
+        }
+    }
+
+    //
+    void CameraScroll()
+    {
+        m_cameraScroll = Input.GetAxis("Mouse ScrollWheel");
+        if (m_cameraScroll != 0)
+        {
+            m_cameraCurSize = m_camera.orthographicSize;
+            m_cameraCurSize -= m_cameraScroll * m_cameraScrollSpeed;
+            m_cameraCurSize = m_cameraCurSize > m_cameraMax ? m_cameraMax : m_cameraCurSize;
+            m_cameraCurSize = m_cameraCurSize < m_cameraMin ? m_cameraMin : m_cameraCurSize;
+            m_camera.orthographicSize = m_cameraCurSize;
+        }
+    }
+}
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float m_speed;
+    public float m_camRayLength = 100;
+
+    private Vector3 movement;
+    private Rigidbody m_Rigidbody;
+    private Ray m_camRay;
+    private int m_floorMask;
+    private Vector3 m_desirdPos;
+    private RaycastHit m_floorHit;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        movement = new Vector3(0f,0f,0f);
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        // Create a layer mask for the floor layer.
+        m_floorMask = LayerMask.GetMask("Ground");
+
+        //
+        m_desirdPos = transform.position;
+    }
+	
+    //
+    void Awake()
+    {
+        m_desirdPos = transform.position;
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        GetInput();
+    }
+
+    //
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    //
+    private void GetInput()
+    {
+        //movement.Set(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Create a ray from the mouse cursor on screen in the direction of the camera.
+            m_camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Perform the raycast and if it hits something on the floor layer...
+            if (Physics.Raycast(m_camRay, out m_floorHit, m_camRayLength, m_floorMask))
+            {
+                // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+                m_desirdPos = m_floorHit.point;
+            }
+        }
+    }
+
+    //
+    private void Move()
+    {
+        movement.Normalize();
+        movement = movement * m_speed * Time.deltaTime;
+
+        if(m_desirdPos != transform.position)
+        {
+            m_Rigidbody.MovePosition(m_desirdPos);
+        }
+    }
+}
